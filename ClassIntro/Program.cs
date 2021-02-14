d401a8a72bba9ead89e2b3f0aa2610e0711bcc85
efdalkolsan <efdalkolsan@gmail.com>
2021-02-14 17:54:31 +0300

    Add project files.

---
+++ ClassIntro/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ClassIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string adi = "Efdal";
+            int yas = 20;
+
+            Kurs kurs1 = new Kurs();
+            kurs1.KursAdi = "C#";
+            kurs1.KursEgitmeni = "Efdal Kolsan";
+            kurs1.KursIzlenmeOrani = 25;
+
+            Kurs kurs2 = new Kurs();
+            kurs2.KursAdi = "Java";
+            kurs2.KursEgitmeni = "Elon Musk";
+            kurs2.KursIzlenmeOrani = 50;
+
+            Kurs kurs3 = new Kurs();
+            kurs3.KursAdi = "Python";
+            kurs3.KursEgitmeni = "Bill Gates";
+            kurs3.KursIzlenmeOrani = 75;
+
+            //Console.WriteLine(kurs1.KursAdi + ":" + kurs1.KursEgitmeni);
+
+            Kurs[] kurslar = new Kurs[] { kurs1, kurs2, kurs3 };
+
+            foreach (var kurs in kurslar)
+            {
+                Console.WriteLine(kurs.KursAdi + " : " + kurs.KursEgitmeni);
+            }
+        }
+    }
+
+    class Kurs
+    {
+        public string KursAdi { get; set; }
+        public string KursEgitmeni { get; set; }
+        public int KursIzlenmeOrani { get; set; }
+
+    }
+}
